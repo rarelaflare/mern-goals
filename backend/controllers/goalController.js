ba5b2fa44e680720ae4945ae608e299bf9568523
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-09-29 13:00:14 -0700

    set up controller and routes for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,39 @@
+const asyncHandler = require('express-async-handler')
+
+
+
+
 // @desc Get goals
 // @route Get /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'get goals'})
-}
+})
 
 // @desc Set goals
 // @route Set /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please enter text')
+    }
     res.status(200).json({message: 'set goal'})
-}
+})
 
 // @desc Update goal
 // @route Put /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 // @desc Delete goal
 // @route Delete /api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 
 // to use in application export
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //Creating first route
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+// Express Error Handler
+app.use(errorHandler)
+
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
