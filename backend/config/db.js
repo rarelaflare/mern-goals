da90435543631c3e6297868556a64598e65e8482
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-10-02 00:00:25 -0700

    correct mistake

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`Process.env value: ${process.env.MONGO_URI}`)
+        
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
     } catch (error) {
         console.log(error);
