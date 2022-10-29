c01d7182a61608531d022f84af52d2410c39d498
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-10-29 05:36:47 -0700

    added user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,32 @@
+/*
+    @desc Register new User
+    @route POST /api/users
+    @access Public
+*/
 const registerUser = (req, res) => {
     res.json({message: 'Register User'})
 }
 
+/*
+    @desc Register new User
+    @route POST /api/users/login
+    @access Public
+*/
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'})
+}
 
+/*
+    @desc Get user data
+    @route POST /api/users/me
+    @access Public
+*/
+const getMe = (req, res) => {
+    res.json({message: 'User Data'})
+}
 
 module.exports = {
     registerUser,
+    loginUser,
+    getMe,
 }
