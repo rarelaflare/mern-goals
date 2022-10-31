b8ae30507fbf624895c779ea6afe252bfe66e7c6
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-10-31 11:01:10 -0700

    added notes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,6 @@ const {registerUser, loginUser, getMe} = require('../controllers/userController'
 */
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.post('/me', getMe)
+router.get('/me', getMe)
 
 module.exports = router
