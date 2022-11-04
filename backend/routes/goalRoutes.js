12d814202fa1aa18a484a1de4f284e03d383b452
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2022-11-04 08:24:26 -0700

    update auth

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -17,10 +17,12 @@ const {
     deleteGoal
 } = require('../controllers/goalController')
 
+const { protect } = require('../middleware/authMiddleware')
+
+
 // Path plus callback functions
+router.route('/').get( protect, getGoals).post( protect, setGoal );
 
-router.route('/').get(getGoals).post(setGoal);
-
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/:id').put( protect, updateGoal ).delete( protect, deleteGoal );
 
 module.exports = router
